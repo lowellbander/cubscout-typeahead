685c44a2ef39c592eba55954f494ae64eb599545
Lowell Bander <lowellbander@gmail.com>
2014-05-30 23:11:28 -0700

    simple state demo working

--- run.py
+++ run.py
@@ -1,24 +1,14 @@
 import os
 
 from flask import Flask
-from flask.ext.mongorest.views import ResourceView
-from flask.ext.mongorest.resources import Resource
-from flask.ext.mongorest import operators as ops
-from flask.ext.mongorest import methods
-
 from flask import render_template
 
 app = Flask(__name__)
 app.debug = True
 
-
-db = MongoEngine(app)
-api = MongoRest(app)
-
 @app.route('/')
 def index():
-    return "hello world"
-    return render_template('cover.html')
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
